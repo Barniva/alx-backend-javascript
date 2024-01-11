bbda40edd78914dc15a49cfd8dcccaa4bd96f73a
Barniva <barnabasva1@gmail.com>
2024-01-11 21:35:45 +0300

    Creating an interface for a student

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,48 +1,87 @@
-interface Student {
-    firstName: string,
-    lastName: string,
-    age: number,
-    location: string
+export interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
 }
+const studentA: Student = {
+  firstName: "Harry",
+  lastName: "Yoon",
+  age: 22,
+  location: "Seoul, South Korea",
+};
+const studentB: Student = {
+  firstName: "Anjali",
+  lastName: "Prajapati",
+  age: 22,
+  location: "Lodz, Poland",
+};
 
-const [student1, student2] = [
-    {
-        firstName: "Gon",
-        lastName: "Freecs",
-        age: 12,
-        location: "New-York"
-    },
-    {
-        firstName: "Suzan",
-        lastName: "Storm",
-        age: 30,
-        location: "Amsterdam"
-    }
-]
+const studentsList: Array<Student> = [
+  studentA,
+  studentB,
+];
+const styleSheet = `
+  html {
+    margin: 0;
+    height: 100%;
+  }
+  body {
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 80%;
+    margin: 10%;
+  }
+  table {
+    border-collapse: collapse;
+  }
+  thead {
+    font-weight: bold;
+  }
+  td {
+    padding: 10px;
+    border: 1px solid gray;
+    cursor: pointer;
+  }
+  td:hover {
+    background: gainsboro;
+  }
 
-const studentsList : Array<Student> = [student1, student2];
+  td:nth-child(1) {
+    text-align: center;
+  }
+`;
 
-const body: HTMLBodyElement = document.getElementsByTagName("body")[0];
-const table : HTMLTableElement = document.createElement("table");
-const thead : HTMLTableSectionElement = document.createElement("thead");
-const tbody : HTMLTableSectionElement = document.createElement("tbody");
-const rowHead: HTMLTableRowElement = thead.insertRow(0);
-const firstCellHead : HTMLTableCellElement = rowHead.insertCell(0);
-const secondCellHead: HTMLTableCellElement = rowHead.insertCell(1);
+/**
+ * Displays information about students in a table.
+ * @param students The list of students to display.
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ */
+export const displayStudents = (students: Array<Student>): void => {
+  const table = document.createElement('table');
+  const tableHead = document.createElement('thead');
+  const headRow = document.createElement('tr');
+  const tableBody = document.createElement('tbody');
+  headRow.insertAdjacentHTML('beforeend', '<td>FirstName</td');
+  headRow.insertAdjacentHTML('beforeend', '<td>Location</td');
+  tableHead.insertAdjacentElement('beforeend', headRow);
 
-firstCellHead.innerHTML = "firstName";
-secondCellHead.innerHTML = "location";
-table.append(thead);
+  for (const student of students) {
+    const bodyRow = document.createElement('tr');
+    bodyRow.insertAdjacentHTML('beforeend', `<td>${student.firstName}</td>`);
+    bodyRow.insertAdjacentHTML('beforeend', `<td>${student.location}</td>`);
+    tableBody.insertAdjacentElement('beforeend', bodyRow);
+  }
 
+  table.insertAdjacentElement('beforeend', tableHead);
+  table.insertAdjacentElement('beforeend', tableBody);
+  document.body.insertAdjacentElement('beforeend', table);
+};
 
-studentsList.forEach(student => {
-    const row : HTMLTableRowElement = tbody.insertRow(0);
-    const firstCell : HTMLTableCellElement = row.insertCell(0);
-    const secondCell : HTMLTableCellElement = row.insertCell(1);
-
-    firstCell.innerHTML = student.firstName;
-    secondCell.innerHTML = student.location;
-});
-
-table.append(tbody);
-body.append(table);
+displayStudents(studentsList);
+const styleSheetElement = document.createElement('style');
+styleSheetElement.innerHTML = styleSheet;
+document.head.insertAdjacentElement('beforeend', styleSheetElement);
+document.title = 'Task 0';
